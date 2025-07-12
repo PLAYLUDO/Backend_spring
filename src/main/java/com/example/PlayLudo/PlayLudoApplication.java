390faace8a7bc01a7b83971afc9a99087e9bf635
S B <sb@Ss-MacBook-Air.local>
2025-07-12 23:05:27 +0530

    Project Uploaded

---
+++ src/main/java/com/example/PlayLudo/PlayLudoApplication.java
@@ -0,0 +1,13 @@
+package com.example.PlayLudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayLudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayLudoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PlayLudo/PlayLudoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PlayLudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayLudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
